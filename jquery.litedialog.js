9a5875b7f610cd257a7bbc95c20ae75d02b1c9df
Howard Yeend <puremango.co.uk@gmail.com>
2011-04-04 02:00:02 +0100

    stripped a few more bytes

--- jquery.litedialog.js
+++ jquery.litedialog.js
@@ -30,48 +30,48 @@
             }
             
             // create shadow if not already added to DOM
-            if($('#'+prefix+'shadow').length === 0) {
-                $("<div id='"+prefix+"shadow' style='position:fixed;top:0;left:0;'>").hide().css({
+            if($('#'+prefix+'Shadow').length === 0) {
+                $("<div id='"+prefix+"Shadow' style='position:fixed;top:0;left:0;'>").hide().css({
                     "height" : $(document).height(),
                     "width" : $(document).width()
                 }).appendTo(document.body);
             }
             
             // create dialog if not already added to DOM
-            if($('#'+prefix+'dialog').length === 0) {
-                $("<div id='"+prefix+"dialog' style='position:absolute;'>").hide().appendTo(document.body);
+            if($('#'+prefix+'Dialog').length === 0) {
+                $("<div id='"+prefix+"Dialog' style='position:absolute;'>").hide().appendTo(document.body);
             }
             
             // fade shadow in (i.e. fade page out to black). Settings could vary with each call, so we set the CSS here not on element creation.
-            $('#'+prefix+'shadow').css({'background':settings.shadow,'z-index':settings.zIndex}).fadeTo('fast',0.4);
+            $('#'+prefix+'Shadow').css({'background':settings.shadow,'z-index':settings.zIndex}).fadeTo('fast',0.4);
             
             // fade dialog in, centered on page.
-            $('#'+prefix+'dialog').html(settings.html).width(settings.width).css({
+            $('#'+prefix+'Dialog').html(settings.html).width(settings.width).css({
                 'box-shadow' : '0px 0px '+settings.borderRadius+' '+settings.shadow,
                 '-moz-box-shadow' : '0px 0px '+settings.borderRadius+' '+settings.shadow,
                 '-webkit-box-shadow' : '0px 0px '+settings.borderRadius+' '+settings.shadow,
                 'color' : settings.color,
                 'background' : settings.background,
                 'padding' : settings.padding,
-                'top' : ($(window).height() - $('#'+prefix+'dialog').outerHeight()) / 2 + $(window).scrollTop(),
-                'left' : ($(window).width() - $('#'+prefix+'dialog').outerWidth()) / 2 + $(window).scrollLeft(),
+                'top' : ($(window).height() - $('#'+prefix+'Dialog').outerHeight()) / 2 + $(window).scrollTop(),
+                'left' : ($(window).width() - $('#'+prefix+'Dialog').outerWidth()) / 2 + $(window).scrollLeft(),
                 'z-index':settings.zIndex+1
             }).fadeIn();
             
             if(!settings.modal) {
               // hit ESC or click anywhere to dismiss if not modal
-              $('#'+prefix+'shadow, #'+prefix+'dialog').click(function() {
+              $('#'+prefix+'Shadow, #'+prefix+'Dialog').click(function() {
                 $.liteDialog('hide');
               });
               $(document).keyup(escapeHitHide);
             } else {
               // remove handlers that may have been present from previously shown dialogs
-              $('#'+prefix+'shadow, #'+prefix+'dialog').unbind();
+              $('#'+prefix+'Shadow, #'+prefix+'Dialog').unbind();
               $(document).unbind("keyup", escapeHitHide);
             }
         },
         hide : function() {
-            $('#'+prefix+'shadow, #'+prefix+'dialog').fadeOut();
+            $('#'+prefix+'Shadow, #'+prefix+'Dialog').fadeOut();
         }
     };
     $.liteDialog = $.fn.liteDialog = function( method ) {
